c22cb0bcf87094c81d55c4be5f72d3ccdf951c9d
Nuklles <103897836+Nuklles@users.noreply.github.com>
2023-04-17 09:34:06 -0300

    Update principal.php

--- principal.php
+++ principal.php
@@ -1,39 +1,79 @@
-<?php 
-
+<?php
 session_start();
-
+include('validar_usuario.php');
 include('conexao.php');
-include('valida_usuario.php');
-
 $matricula = $_SESSION['matricula'];
-
-$select =  "SELECT nome FROM cadastro 
-			WHERE matricula = '$matricula'";
+$select = "select  nome from cadastro where matricula = '$matricula' ";
 $query = mysqli_query($conexao, $select);
 $dado = mysqli_fetch_row($query);
 
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Principal</title>
+	<title>Menu</title>
+	<style>
+		body {
+			font-family: Arial, sans-serif;
+			background-color: #f2f2f2;
+		}
+		h1 {
+			text-align: center;
+			font-size: 36px;
+			color: #333;
+			margin-top: 30px;
+		}
+		ul {
+			list-style: none;
+			padding: 0;
+			margin: 50px auto;
+			display: table;
+		}
+		li {
+			display: table-row;
+			margin-bottom: 20px;
+			background-color: #fff;
+			border-radius: 5px;
+			box-shadow: 0 2px 5px rgba(0,0,0,0.1);
+			transition: all 0.2s ease-in-out;
+		}
+		li:hover {
+			transform: scale(1.05);
+			box-shadow: 0 5px 15px rgba(0,0,0,0.2);
+		}
+		a {
+			display: table-cell;
+			vertical-align: middle;
+			padding: 20px;
+			color: #333;
+			text-decoration: none;
+			font-size: 24px;
+		}
+		a:hover {
+			color: #0077b5;
+			text-decoration: underline;
+		}
+		.logout {
+			display: block;
+			margin: 0 auto;
+			text-align: center;
+			font-size: 18px;
+			color: #999;
+			margin-top: 50px;
+		}
+	</style>
 </head>
 <body>
-	<center>
-	<h1>Bem vindo, <?php echo $dado[0]; ?>!!</h1>
-	<nav>
-		<a href="consultar.php">Consultar Usuário</a><br>
-		<a href="cadastrar.php">Cadastrar Usuário</a><br>
-		<a href="alterar_senha.php">Alterar Senha</a><br>
-		<a href="pesquisar_cnpj.php">Pesquisar CNPJ</a><br>
-		<a href="gerar_json.php">Gerar arquivo Json do Cadastro</a><br>
-		<a href="cadastro.json" target="_blank">Exibir Arquivo Json Gerado</a><br>
-		<a href="enviar_cadastro" target="_blank">Enviar Cadastro</a><br>
-		<a href="sair.php">Sair</a>
-	</nav>
-	</center>
+	<h1>Menu</h1>
+	
+	<ul>
+		<li><a href="consultar.php">Consultar usuários</a></li>
+		<li><a href="cadastrar.php">Cadastrar novo usuário</a></li>
+		<li><a href="alterarSenha.php">Alterar sua senha</a></li>
+		<li><a href="pesquisarcnpj.php">Pesquisar CNPJ</a></li>
+		<li><a href="EnviarCadastro.php">Enviar cadastro</a></li>
+		<li><a href="Ver_alunos_form.php">Ver Alunos</a></li>
+		</ul>
+	<div class="logout"><a href="sair.php">Sair</a></div>
 </body>
 </html>
