e54ac1fb5beeb80b98e9581cd334ade10c000548
Nuklles <103897836+Nuklles@users.noreply.github.com>
2023-04-17 09:27:23 -0300

    Update autenticar.php

--- autenticar.php
+++ autenticar.php
@@ -1,28 +1,37 @@
 <?php
-
-include('conexao.php');
-include('valida_usuario.php');
+session_start();
+include ('conexao.php');
+include('validar_usuario.php');
 
 $login = isset($_POST['login']) ? $_POST['login'] : '';
 $senha = isset($_POST['senha']) ? $_POST['senha'] : '';
 
-$select = "SELECT matricula, login, senha FROM login
-			WHERE login = '$login' AND senha = '$senha'";
-/*
-POSTGRES
-$query = pg_exec($conexao, $select);
-$dado = pg_fetch_row($query);
-*/
+$select = "SELECT matricula, login, senha FROM login 
+            WHERE login = '$login' AND senha = '$senha'";
+
+
 $query = mysqli_query($conexao, $select);
 $dado = mysqli_fetch_row($query);
 
 if ($login == isset($dado[1]) && $senha == isset($dado[2])) {
-	session_start();
-	$_SESSION['matricula'] = $dado[0];
-	header ("location: principal.php");
-}
-else {
-	header ("location: index.php");
+    session_start();
+    $_SESSION['matricula'] = $dado[0];
+        header ("location: principal.php");
 }
 
+else    {
+    
+    echo "Usuario ou senha inválidos";
+   
+
+}
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+    <br><br>
+    <a href="index.php">Voltar para o Menu Principal</a>
+</body>
+</html>
